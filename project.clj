f51dc8386052963afaac92e787aece461ffb0856
Divyansh Prakash <divyansh.prakash@yahoo.com>
2022-11-25 14:19:38 +0530

    Fix cloud build

--- project.clj
+++ project.clj
@@ -4,10 +4,14 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
-  :main ^:skip-aot simple-stackless-lisp.core
+  :main simple-stackless-lisp.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :native-image {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
   :plugins [[io.taylorwood/lein-native-image "0.3.1"]]
   :native-image {:name "sclj"
-                 :opts ["--verbose"]})
+                 :opts ["--verbose"
+                        "--no-fallback"
+                        "--report-unsupported-elements-at-runtime"
+                        "--initialize-at-build-time"
+                        "--no-server"]})
