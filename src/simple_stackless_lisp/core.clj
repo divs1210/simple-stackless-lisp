536e96339821be2fe2dd7be10071c8008ca0627b
Divyansh Prakash <divyansh@Apples-MacBook-Pro.local>
2022-12-01 16:53:11 +0530

    Bugfix: walk GUARD was called with wrong number of args

--- src/simple_stackless_lisp/core.clj
+++ src/simple_stackless_lisp/core.clj
@@ -6,61 +6,60 @@
    [simple-stackless-lisp.util :as u :refer [->cps]]))
 
 (defn walk
-  ([exp env k GUARD]
-   (walk walk exp env k GUARD))
-  ([this exp env k GUARD]
-   (GUARD this [exp env k GUARD])
-   (cond
-     (or (number? exp)
-         (string? exp))
-     (k exp)
+  [this exp env k GUARD]
+  (GUARD this [this exp env k GUARD])
+  (cond
+    (or (number? exp)
+        (string? exp))
+    (k exp)
 
-     (contains? #{nil true false} exp)
-     (k exp)
+    (contains? #{nil true false} exp)
+    (k exp)
 
-     (symbol? exp)
-     (k (env/lookup env exp))
+    (symbol? exp)
+    (k (env/lookup env exp))
 
-     (seq? exp)
-     (let [[op & args] exp]
-       (case op
-         def
-         (impl/k-def this args env k GUARD)
+    (seq? exp)
+    (let [[op & args] exp]
+      (case op
+        def
+        (impl/k-def this args env k GUARD)
 
-         let
-         (impl/k-let this args env k GUARD)
+        let
+        (impl/k-let this args env k GUARD)
 
-         if
-         (impl/k-if this args env k GUARD)
+        if
+        (impl/k-if this args env k GUARD)
 
-         do
-         (impl/k-do this args env k GUARD)
+        do
+        (impl/k-do this args env k GUARD)
 
-         quote
-         (impl/k-quote this args env k GUARD)
+        quote
+        (impl/k-quote this args env k GUARD)
 
-         fn
-         (impl/k-fn this [args false] env k GUARD)
+        fn
+        (impl/k-fn this [args false] env k GUARD)
 
-         macro
-         (impl/k-fn this [args true] env k GUARD)
+        macro
+        (impl/k-fn this [args true] env k GUARD)
 
-         trace!
-         (impl/k-trace! this args env k GUARD)
+        trace!
+        (impl/k-trace! this args env k GUARD)
 
-         eval
-         (impl/k-eval this args env k GUARD)
+        eval
+        (impl/k-eval this args env k GUARD)
 
-         ;; function call
-         (impl/k-apply this [op args] env k GUARD)))
+        ;; function call
+        (impl/k-apply this [op args] env k GUARD)))
 
-     :else
-     (u/throw+ "Can't evaluate: " exp))))
+    :else
+    (u/throw+ "Can't evaluate: " exp)))
 
 (def builtins
   {'list  (->cps list)
    'first (->cps first)
    'rest  (->cps rest)
+   'next  (->cps next)
    'seq   (->cps seq)
    'seq?  (->cps seq?)
    'cons  (->cps cons)
@@ -101,4 +100,4 @@
    (eval exp env k (u/executor)))
   ([exp env k exe]
    (let [{:keys [guard execute]} exe]
-     (execute walk [exp env k guard]))))
+     (execute walk [walk exp env k guard]))))
