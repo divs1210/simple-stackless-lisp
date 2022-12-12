e28f4d83463b89b7b3c7fe18bc4e7af23c1ca3fe
Divyansh Prakash <divyansh@Apples-MacBook-Pro.local>
2022-12-12 21:31:24 +0530

    Add profiler, interpreter is 25x (!) slower after adding namespaces

--- project.clj
+++ project.clj
@@ -6,13 +6,18 @@
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :main simple-stackless-lisp.main
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
+  :profiles {:dev {:dependencies [[com.clojure-goes-fast/clj-async-profiler "1.0.3"]]
+                   :global-vars {*warn-on-reflection* true}
+                   :jvm-opts ["-XX:+UnlockDiagnosticVMOptions"
+                              "-XX:+DebugNonSafepoints"
+                              "-Djdk.attach.allowAttachSelf=true"]}
+             :uberjar {:aot :all
                        :global-vars {*warn-on-reflection* true}}
              :native-image {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
   :plugins [[io.taylorwood/lein-native-image "0.3.1"]]
   :native-image {:name "sclj"
                  :opts ["--verbose"
                         "--no-fallback"
-                        "--report-unsupported-elements-at-runtime"
+                        ;; "--report-unsupported-elements-at-runtime"
                         "--initialize-at-build-time"
                         "--no-server"]})
