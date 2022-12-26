027e2714f99f1d94857252a2c4f5e2009f09f970
Divyansh Prakash <divyansh@Apples-MacBook-Pro.local>
2022-12-26 19:10:20 +0530

    Refactor main.clj

--- src/simple_stackless_lisp/main.clj
+++ src/simple_stackless_lisp/main.clj
@@ -3,23 +3,24 @@
   (:refer-clojure :exclude [eval])
   (:require
    [clojure.edn :as edn]
+   [clojure.java.io :as io]
    [simple-stackless-lisp.core :as core]
    [simple-stackless-lisp.env :as env]
-   [simple-stackless-lisp.util :as u]
-   [clojure.java.io :as io]))
+   [simple-stackless-lisp.util :as u]))
 
 (defn run-file
   [filename]
   (try
     (let [file (io/file filename)
-          _ (when-not (.exists file)
-              (u/throw+ "File not found:" filename))
+          _    (when-not (.exists file)
+                 (u/throw+ "File not found:" filename))
           cwd  (-> file .getAbsoluteFile .getParent)
           text (str "(do " (slurp file) ")")
           code (edn/read-string text)
-          ns-reg (env/fresh-ns-registry)
-          _ (env/create-ns! ns-reg 'sclj.core core/builtins)
-          _ (env/set-current-wd! ns-reg cwd)]
+          ns-reg (env/fresh-ns-registry)]
+      (env/create-ns! ns-reg 'sclj.core core/builtins)
+      (env/create-ns! ns-reg 'user)
+      (env/set-current-wd! ns-reg cwd)
       (core/eval code ns-reg))
     (catch Exception e
       (println "Error: " (.getMessage e))
@@ -27,26 +28,23 @@
       (spit "sclj-err-log.edn" (pr-str e)))))
 
 (defn run-repl []
-  (let [ns-reg (env/fresh-ns-registry)
-        env (env/create-ns! ns-reg 'sclj.core core/builtins)
-        k   (fn [ret]
-              (env/bind! env '*1 ret)
-              (println "=>" (pr-str ret) "\n"))
-        exe (u/executor)
-        eval (fn [exp k]
-               (core/eval exp ns-reg k exe))]
-    (println "=====================================================")
-    (println "|            Simple Stackless Lisp REPL             |")
-    (println "|---------------------------------------------------|")
-    (println "| https://github.com/divs1210/simple-stackless-lisp |")
-    (println "=====================================================")
+  (println "=====================================================")
+  (println "|            Simple Stackless Lisp REPL             |")
+  (println "|---------------------------------------------------|")
+  (println "| https://github.com/divs1210/simple-stackless-lisp |")
+  (println "=====================================================")  
+  (let [ns-reg (env/fresh-ns-registry)]
+    (env/create-ns! ns-reg 'sclj.core core/builtins)
+    (env/create-ns! ns-reg 'user)
     (while true
       (try
-        (print (str (env/current-ns env) "> "))
+        (print (str (env/current-ns ns-reg) "> "))
         (flush)
-        (eval (u/read-exp) k)
+        (let [ret (core/eval (u/read-exp) ns-reg)]
+          (env/bind! (env/current-env ns-reg) '*1 ret)
+          (println "=>" (pr-str ret) "\n"))
         (catch Exception e
-          (env/bind! env '*e e)
+          (env/bind! (env/current-env ns-reg) '*e e)
           (println "Error: " (.getMessage e)))))))
 
 (defn -main
