e065b336882b339d928842f1d39c1d96784cfe71
Divyansh Prakash <divyansh@Apples-MacBook-Pro.local>
2023-01-06 21:04:56 +0530

    Comment out types experiment

--- src/experiments/types.clj
+++ src/experiments/types.clj
@@ -4,43 +4,43 @@
 
 ;; Interface
 ;; =========
-(defmulti get
-  (fn [this & _]
-    (t/type this)))
+;; (defmulti get
+;;   (fn [this & _]
+;;     (t/type this)))
 
-(defmulti slice
-  (fn [this & _]
-    (t/type this)))
+;; (defmulti slice
+;;   (fn [this & _]
+;;     (t/type this)))
 
-(defmulti put!
-  (fn [this & _]
-    (t/type this)))
+;; (defmulti put!
+;;   (fn [this & _]
+;;     (t/type this)))
 
 
 ;; Implementations
 ;; ===============
-(defmethod get 'Array
-  [this idx]
-  (t/array-get this idx))
+;; (defmethod get 'Array
+;;   [this idx]
+;;   (t/array-get this idx))
 
-(defmethod slice 'Array
-  [this from to]
-  (t/array-slice this from to))
+;; (defmethod slice 'Array
+;;   [this from to]
+;;   (t/array-slice this from to))
 
-(defmethod put! 'Array
-  [this idx val]
-  (t/array-put! this idx val))
+;; (defmethod put! 'Array
+;;   [this idx val]
+;;   (t/array-put! this idx val))
 
-;; ===
+;; ;; ===
 
-(defmethod get 'ArrayWindow
-  [this idx]
-  (t/array-window-get this idx))
+;; (defmethod get 'ArrayWindow
+;;   [this idx]
+;;   (t/array-window-get this idx))
 
-(defmethod put! 'ArrayWindow
-  [this idx val]
-  (t/array-window-put! this idx val))
+;; (defmethod put! 'ArrayWindow
+;;   [this idx val]
+;;   (t/array-window-put! this idx val))
 
-(defmethod slice 'ArrayWindow
-  [this from to]
-  (t/array-window-slice this from to))
+;; (defmethod slice 'ArrayWindow
+;;   [this from to]
+;;   (t/array-window-slice this from to))
